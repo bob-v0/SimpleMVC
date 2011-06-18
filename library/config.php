ddadce4ab83f79d9d12a33033bcfc5b5cb499f64
dutchpowercow@gmail.com <dutchpowercow@gmail.com>
2011-06-18 20:06:53 +0200

    env details added

---
+++ library/config.php
@@ -0,0 +1,22 @@
+<?php
+
+class Config
+{
+    public static function setEnvironmentSettings()
+    {
+        if (DEV_ENV) {
+            error_reporting(E_ALL);
+            ini_set('display_errors', 'On');
+        } else {
+            error_reporting(E_ALL);
+            ini_set('display_errors', 'Off');
+            ini_set('log_errors', 'On');
+            ini_set('error_log', ROOT . DS . 'logs' . DS . 'error.log');
+        }
+    }
+
+    public static function getModules()
+    {
+        return array('default', 'module1');
+    }
+}
