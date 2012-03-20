9079ea484c91857ef564efff33eac06caab743c2
vdm <b.vandam@telis.nl>
2012-03-20 23:32:22 +0100

    sample file added

---
+++ application/default/controller/sample.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: vdm
+ * Date: 20-3-12
+ * Time: 23:27
+ * To change this template use File | Settings | File Templates.
+ */
+class SampleController extends Controller
+{
+    public function indexAction()
+    {
+        echo "Hello cruel world!";
+    }
+}
